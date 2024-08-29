6660a15a9783524e7624df9174e61b36bf56de83
Liubou Levitskaya <lyubasaraeva@gmail.com>
2024-08-29 17:42:16 +0300

    feat: realize reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let number = n.toString();
+    if (number[0] === '-') {
+        return number.slice(1).split('').reverse().join('');
+    } else {
+        return number.split('').reverse().join('');
+    }
 }
+
